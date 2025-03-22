22d371908c671a2ca6fe222e0a2a62bbd0048c46
Joshua <dev.joshuadolor@gmail.com>
2025-03-22 05:38:14 +0100

    use mailhog

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+/*
+use Illuminate\Support\Facades\Mail;
+Route::get('/test-email', function() {
+    Mail::raw('Test email content', function($message) {
+        $message->to('test@example.com')
+                ->subject('Test Email');
+    });
+    return 'Email sent successfully!';
+});
+*/
 
 // Catch all route for SPA
 Route::get('{any}', function () {
     return view('app');
 })->where('any', '.*');
+
