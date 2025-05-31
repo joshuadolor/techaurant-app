033373402660a9f16e212d1a82fb1106a08dda7f
Joshua Dolor <dev.joshuadolor@gmail.com>
2025-05-31 15:01:03 +0200

    explicitly declare config as int

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -70,8 +70,8 @@ class AuthController extends Controller
 
     private function sendResponseWithTokens(array $tokens, $body = []): JsonResponse
     {
-        $rtExpireTime = config('sanctum.refresh_token_expiration');
-        $cookie = cookie('refreshToken', $tokens['refreshToken'], $rtExpireTime, secure: true);
+        $rtExpireTime = (int) config('sanctum.refresh_token_expiration');
+        $cookie = cookie('refreshToken', $tokens['refreshToken'], $rtExpireTime, '/', null, true, true);
 
         return $this->successResponse(array_merge($body, [
             'token' => $tokens['accessToken']
